b989ae0a01d9dcab36012291f7ecf52f420dcc61
Kirbologist <siwanli1337@gmail.com>
2022-01-09 23:58:27 +1000

    Fixed library compatibility

--- src/raman_elastic_scattering.mk
+++ src/raman_elastic_scattering.mk
@@ -1,12 +1,10 @@
 
-UNSUPPORTED_FLAG=-I/lib/include/eigen3/unsupported
-EIGEN_FLAG=
-GMP_FLAG=
-MPFR_FLAG=
-BOOST_FLAG=
+UNSUPPORTED_FLAG=-I../lib/eigen-3.4.0/unsupported
+EIGEN_FLAG=-I../lib/eigen-3.4.0
+BOOST_FLAG=I../lib/boost_1_77_0
 
 CC=g++
-CPPFLAGS=-std=c++17 -Wall -pedantic -g -msse2 -O2 $(UNSUPPORTED_FLAG) $(EIGEN_FLAG) $(GMP_FLAG) $(MPFR_FLAG) $(BOOST_FLAG)
+CPPFLAGS=-std=c++17 -Wall -pedantic -g -msse2 -O2 $(UNSUPPORTED_FLAG) $(EIGEN_FLAG) $(BOOST_FLAG)
 OBJS=raman_elastic_scattering.o math.o raman_aux.o vsh.o sph.o rvh.o slv.o pst.o
 PRODUCT=raman_elastic_scattering
 
